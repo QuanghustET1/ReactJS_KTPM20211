83d2faac63ad6eaca10dcfcfd6e0fc990841edad
Nguyễn Văn Quang <Quang.nv193069@sis.hust.edu.vn>
2021-12-25 22:10:41 +0700

    Video #57

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { getAllcodeService } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
+import * as actions from "../../../store/actions";
 
 class UserRedux extends Component {
 
@@ -17,23 +18,19 @@ class UserRedux extends Component {
     }
 
     async componentDidMount() {
-        try {
-            let res = await getAllcodeService('gender');
-            if (res && res.errCode === 0) {
-                this.setState({
-                    genderArr: res.data
-                })
-            }
-            console.log(res)
-        } catch (e) {
-            console.log(e)
+        this.props.getGenderStart();
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.genderRedux !== this.props.genderRedux) {
+            this.setState({
+                genderArr: this.props.genderRedux
+            })
         }
     }
 
 
 
     render() {
-        console.log(this.state)
         let genders = this.state.genderArr;
         let language = this.props.language
         return (
@@ -43,7 +40,7 @@ class UserRedux extends Component {
                 </div>
                 <div className="user-redux-body">
                     <div className="container">
-                        <div className="row" style={{display: "grid"}}>
+                        <div className="row" style={{ display: "grid" }}>
                             <form>
                                 <div className="form-row">
                                     <div className="form-group col-md-3">
@@ -134,11 +131,13 @@ class UserRedux extends Component {
 const mapStateToProps = state => {
     return {
         language: state.app.language,
+        genderRedux: state.admin.genders
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        getGenderStart: () => dispatch(actions.fetchGenderStart())
     };
 };
 
---
+++ src/store/actions/adminActions.js
@@ -0,0 +1,24 @@
+import actionTypes from "./actionTypes";
+import { getAllcodeService } from "../../services/userService";
+export const fetchGenderStart = () => {
+    return async (dispatch, getState) => {
+        try {
+            let res = await getAllcodeService("GENDER");
+            if (res && res.errCode === 0) {
+                dispatch(fetchGenderSuccess(res.data))
+            } else {
+                dispatch(fetchGenderFailed())
+            }
+        } catch (e) {
+            dispatch(fetchGenderFailed());
+            console.log('fetch gender error', e)
+        }
+    }
+}
+export const fetchGenderSuccess = (genderData) => ({
+    type: actionTypes.FETCH_GENDER_SUCCESS,
+    data: genderData
+})
+export const fetchGenderFailed = () => ({
+    type: actionTypes.FETCH_GENDER_FAIDED
+})
